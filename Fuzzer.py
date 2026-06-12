9380ebdc1fbc1e2a33d4a96a92225e0d47ed01a4
The Final Bit <203424434+thefinalbit@users.noreply.github.com>
2026-06-12 11:11:09 +0300

    Update Fuzzer.py

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,7 +1,7 @@
 # =================================================================
 # Challenge: whekkees's my first simple crack me by whekkees
 # Challenge Link: https://crackmes.one/crackme/69b18083ddd6176826ae8947
-# Writeup Link: [ضع_رابط_مقال_الـ_Writeup_هنا]
+# Writeup Link: [https://github.com/thefinalbit/ctf-writeups/blob/main/Reverse%20Engineering/whekkees's-my-first-simple-crack-me-by-whekkees.md]
 # Description: This helper script was created with AI assistance 
 #              to fuzz the binary and analyze input crashes.
 # =================================================================
